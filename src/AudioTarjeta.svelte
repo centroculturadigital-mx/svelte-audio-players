139ec3f4966a3f3111fb0468457e145cdfd844af
furenku <furenku@gmail.com>
2019-10-07 21:17:49 -0500

    iterando entre archivos

--- src/AudioTarjeta.svelte
+++ src/AudioTarjeta.svelte
@@ -2,19 +2,29 @@
 	import { onMount, onDestroy } from 'svelte';
 
     // import audio from "./data/ambiente-interior.ogg"
-    const audio = "/data/ambiente-interior.ogg"
+    const unArchivo = "/data/ambiente-interior.ogg"
 
-    let audioHtml=[];
-    let tocando=[false,false];
-    let tiempo = [0,0]
-    let duracion = [0,0]
-    let progreso = [0,0]
+    let numAudios = 5
+
+    let archivosAudio = new Array(numAudios).fill({unArchivo})
+    
+    let audios = []
+
+    let tocando=new Array(numAudios).fill(false)
+    let tiempo = new Array(numAudios).fill(0)
+    let duracion = new Array(numAudios).fill(0)
+    let progreso = new Array(numAudios).fill(0)
 
     let actualizacion
 
 
     const formatearDuracion = (duracion) => {
         let date = new Date(null);
+        
+        if(Number.isNaN(duracion)) {
+            duracion = 0
+        }
+        
         date.setSeconds(duracion); // specify value for SECONDS here
         return date.toISOString().substr(14, 5);
     }
@@ -23,18 +33,20 @@
 
     const actualizar = ()=>{
             
-        tiempo[0]=formatearDuracion(audioHtml[0].currentTime)
-        
-        duracion[0]= formatearDuracion(audioHtml[0].duration)
 
-        progreso[0]=( audioHtml[0].currentTime/audioHtml[0].duration ) * 100
-         
-         
-        tiempo[1]=formatearDuracion(audioHtml[1].currentTime)
-        
-        duracion[1]= formatearDuracion(audioHtml[1].duration)
+        for( let indice in audios ) {
+            let audio = audios[indice]
 
-        progreso[1]=( audioHtml[1].currentTime/audioHtml[1].duration ) * 100
+            // console.log(audio, indice);
+            
+            tiempo[indice]=formatearDuracion(audio.currentTime)
+            
+            duracion[indice]= formatearDuracion(audio.duration)
+    
+            progreso[indice]=( audio.currentTime/audio.duration ) * 100
+
+        }
+         
 
     }
 
@@ -55,17 +67,17 @@
 
     const tocar = (i) => {
         
-        if( !! audioHtml[i] ) {
+        if( !! audios[i] ) {
             
-            audioHtml[i].play()
+            audios[i].play()
             tocando[i] = true;
             
         }
     }
     const parar = (i) => {
-        if( !! audioHtml[i] ) {
+        if( !! audios[i] ) {
 
-            audioHtml[i].pause()
+            audios[i].pause()
             tocando[i] = false;
         
         }
@@ -84,44 +96,31 @@ input[type="range"] {
 }
 </style>
 
-<article class="AudioTarjeta">
+{#each archivosAudio as archivoAudio, indice ('audio_'+indice) }
+    <article class="AudioTarjeta">
 
-    <header>
-        {#if tocando[0]}
-            <button on:click={()=>parar(0)}>Parar</button>
+        <header>
+        {#if tocando[indice]}
+            <button on:click={()=>parar(indice)}>Parar</button>
         {:else}
-            <button on:click={()=>tocar(0)}>Tocar</button>
-        {/if}
-        {#if tocando[1]}
-            <button on:click={()=>parar(1)}>Parar</button>
-        {:else}
-            <button on:click={()=>tocar(1)}>Tocar</button>
+            <button on:click={()=>tocar(indice)}>Tocar</button>
         {/if}
 
         <div class="Tiempo">
-            <span class="Posicion">{tiempo[0]}</span>
-            <span class="Duracion">{duracion[0]}</span>
-            <input type="range" class="Progreso" value={progreso[0]}/>
+            <span class="Posicion">{tiempo[indice]}</span>
+            <span class="Duracion">{duracion[indice]}</span>
+            <input type="range" class="Progreso" value={progreso[indice]}/>
         </div>
+        </header>
 
-        <div class="Tiempo">
-            <span class="Posicion">{tiempo[1]}</span>
-            <span class="Duracion">{duracion[1]}</span>
-            <input type="range" class="Progreso" value={progreso[1]}/>
-        </div>
-    </header>
-
-    <audio bind:this={audioHtml[0]}>
-        <source src={audio}/>
-    </audio>
-
-    <audio bind:this={audioHtml[1]}>
-        <source src={audio}/>
-    </audio>
+        <audio bind:this={audios[indice]}>
+            <source src={archivoAudio}/>
+        </audio>
 
 
-    <section class="Informacion">
-        Informacion
-    </section>
-    
-</article>
+        <section class="Informacion">
+            Informacion
+        </section>
+        
+    </article>
+{/each}
